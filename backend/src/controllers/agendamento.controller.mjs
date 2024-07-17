6d5927924db5e1116dff09ba5ed52f36466fc79f
Samara Souza <samyesouza13@gmail.com>
2024-07-17 15:51:41 -0300

    :sparkles: feat: Conferindo e ajustando os dados recebidos para armazenamento

--- backend/src/controllers/agendamento.controller.mjs
+++ backend/src/controllers/agendamento.controller.mjs
@@ -4,12 +4,40 @@ import z from 'zod';
 
 const agendSchema = z.object({
     id: z.string().optional(),
-    nome: z.string(),
+    nome: z.string().min(1),
     nascData: z.string(),
     agendData: z.string(),
     status: z.string(),
 });
 
 export default class AgendController {
-   
+    async store(req, res) {
+            const agendamento = req.body;
+            const { success, data, error } = agendSchema.safeParse({
+                nome: agendamento.nome,
+                nascData: agendamento.nascData,
+                agendData: agendamento.agendData,
+                status: agendamento.status,
+            });
+
+            if (!success) {
+                return res.status(400).send(error);
+            }
+            // Tratamento de datas
+            const formattedNascData = agendamento.nascData.split("T")[0];
+            const novoAgendData = new Date(data.agendData);
+            novoAgendData.setHours(novoAgendData.getHours() - 3);
+
+            const adjustedAgendData = novoAgendData.toISOString();
+
+            // Criando id único aleatório
+            const [id] = crypto.randomUUID().split('-');
+
+            const agendamentoData = {
+                id: id,
+                nascData: formattedNascData,
+                agendData: adjustedAgendData,
+                status: agendamento.status,
+            };
+    }
 }
