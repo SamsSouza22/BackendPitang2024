3d917dea45f745e50c0a231278e0671ca5e22f55
Samara Souza <samyesouza13@gmail.com>
2024-07-17 12:40:39 -0300

    :sparkles: feat: Conectando o express com as rotas criadas

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -3,12 +3,15 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import agendRoutes from './routes/agendamento.router.mjs';
+
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(morgan("combined"));
 server.use(express.json());
+server.use(agendRoutes);
 
 server.use((error, req, res, next) => {
     console.error(error.stack);
