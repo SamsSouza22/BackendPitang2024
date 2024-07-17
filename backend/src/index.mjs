904c9ffce34d724e7dea82b62148c0ea77b9a717
Samara Souza <samyesouza13@gmail.com>
2024-07-17 08:35:58 -0300

    :sparkles: feat: Ligando o servidor com as dependências necessárias

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,6 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(morgan("combined"));
+server.use(express.json());
+
+server.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).send({ message: error.message });
+});
 
 server.listen(5000, () => {
     console.log(`Estou rodando na porta 5000`);
